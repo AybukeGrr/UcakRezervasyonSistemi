42da1a650642eec4d076c245c4a2080a6b63e582
AybukeGrr <74709621+AybukeGrr@users.noreply.github.com>
2021-02-23 21:56:40 +0300

    Add files via upload

---
+++ UçakBiletRezervasyonSistemi/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UçakBiletRezervasyonSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text + " Tarih: " + dateTimePicker1.Text + " Saat: " + maskedTextBox1.Text + " Yolcunun Adı: " + textBox1.Text + " Yolcu TC: " + maskedTextBox2.Text + " Yolcu Telefonu: " + maskedTextBox3.Text);
+            MessageBox.Show("Yolcu Kaydı Yapıldı. " +
+                "Bizi seçtiğiniz İçin teşekkür eder, iyi uçuşlar dileriz.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label9.Text;
+        }
+    }
+}
